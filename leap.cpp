a5afb32fbb130fdcf2d5dae6e18938c7e4698bd9
tamannachowdhuryy <104877190+tamannachowdhuryy@users.noreply.github.com>
2023-01-26 18:14:38 -0500

    Update leap.cpp

--- leap.cpp
+++ leap.cpp
@@ -1,19 +1,16 @@
-
 #include <iostream>
-#include <string> 
-using namespace std;
 int main(){
   int year;
-  cout << "Enter year: ";
-  cin >> year; 
+  std::cout << "Enter year: ";
+  std::cin >> year; 
   if (year % 4 != 0){
-    cout << "common year" ;
+    std::cout << "common year" ;
   } else if(year % 100 != 0){
-    cout << "it is a leap year "; 
+    std::cout << "it is a leap year "; 
   } else if (year % 400 !=0){
-    cout << "it it a common year";
+    std::cout << "it it a common year";
   } else{
-    cout << "it is a leap year";
+    std::cout << "it is a leap year";
   }
   return 0;
 }
