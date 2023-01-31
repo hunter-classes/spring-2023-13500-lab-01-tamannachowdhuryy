9b85c52ca7a5863e848ea4d90715c60de053d82b
tamannachowdhuryy <104877190+tamannachowdhuryy@users.noreply.github.com>
2023-01-31 18:37:20 -0500

    Update month.cpp

--- month.cpp
+++ month.cpp
@@ -1,3 +1,12 @@
+/*
+Author: Tamanna Chowdhury
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab1D
+Write a program month.cpp that asks the user to input the year and the month (1-12) and prints the number 
+of days in that month (taking into account leap years). You may not use switch case or arrays even if you know these language constructs.
+*/
+
 #include <iostream>
 int main(){
   int year, month;
